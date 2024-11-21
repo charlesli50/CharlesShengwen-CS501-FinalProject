33ad449dd5cac8286b15430a5235d76338528724
charlesli50 <charles.zhiyu.li@gmail.com>
2024-11-21 18:32:05 -0500

    first commit

---
+++ app/src/main/java/com/example/charlesshengwen_finalproject/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.charlesshengwen_finalproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.charlesshengwen_finalproject.ui.theme.CharlesShengwenFinalProjectTheme
+
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.toArgb
+
+data class ChartModel(
+    val value: Float,
+    val color: Color,
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CharlesShengwenFinalProjectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+val charts = listOf(
+    ChartModel(value = 20f, color = Color.Black),
+    ChartModel(value = 30f, color = Color.Gray),
+    ChartModel(value = 40f, color = Color.Green),
+    ChartModel(value = 10f, color = Color.Red),
+)
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    ChartCirclePie(
+        modifier = modifier,
+        charts = charts
+    )
+}
+
+
+@Composable
+private fun ChartCirclePie(
+    modifier: Modifier,
+    charts: List<ChartModel>,
+    size: Dp = 200.dp,
+    strokeWidth: Dp = 16.dp
+) {
+    Canvas(modifier = modifier
+        .size(size)
+//        .background(Color.LightGray)
+        .padding(12.dp),
+
+        onDraw = {
+
+            var startAngle = 0f
+            var sweepAngle = 0f
+
+            charts.forEach {
+
+                sweepAngle = (it.value / 100) * 360
+
+                drawArc(
+                    color = it.color,
+                    startAngle = startAngle,
+                    sweepAngle = sweepAngle,
+                    useCenter = false,
+                    style = Stroke(
+                        width = strokeWidth.toPx(),
+                        cap = StrokeCap.Round,
+                        join = StrokeJoin.Round
+                    )
+                )
+
+                startAngle += sweepAngle
+            }
+
+        })
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CharlesShengwenFinalProjectTheme {
+        Greeting("Android")
+    }
+}
